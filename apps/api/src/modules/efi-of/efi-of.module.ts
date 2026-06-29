6c522778841fe90aeb41ef56ea18e8cd2a4db910
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-06-28 23:21:45 -0300

    feat: register efi open finance diagnostic controller

--- apps/api/src/modules/efi-of/efi-of.module.ts
+++ apps/api/src/modules/efi-of/efi-of.module.ts
@@ -3,10 +3,16 @@ import { EfiOFService } from './efi-of.service';
 import { EfiOFWebhookController } from './efi-of-webhook.controller';
 import { EfiOFWebhookReceiverController } from './efi-of-webhook-receiver.controller';
 import { EfiWebhookPublicController } from './efi-webhook-public.controller';
+import { EfiOFDebugController } from './efi-of-debug.controller';
 
 @Module({
   providers: [EfiOFService],
-  controllers: [EfiOFWebhookController, EfiOFWebhookReceiverController, EfiWebhookPublicController],
+  controllers: [
+    EfiOFWebhookController,
+    EfiOFWebhookReceiverController,
+    EfiWebhookPublicController,
+    EfiOFDebugController
+  ],
   exports: [EfiOFService]
 })
 export class EfiOFModule {}
