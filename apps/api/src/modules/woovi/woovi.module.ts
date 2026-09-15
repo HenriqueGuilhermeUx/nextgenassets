715bed582799382efdfa9f754b29f8b99aac23e7
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-15 15:57:04 -0300

    Register NexOffice charge bridge

--- apps/api/src/modules/woovi/woovi.module.ts
+++ apps/api/src/modules/woovi/woovi.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WooviPixAdapter } from './woovi-pix-adapter';
 import { WooviWebhookController } from './woovi-webhook.controller';
+import { NexOfficeChargeController } from './nexoffice-charge.controller';
 
 @Module({
   providers: [WooviPixAdapter],
-  controllers: [WooviWebhookController],
+  controllers: [WooviWebhookController, NexOfficeChargeController],
   exports: [WooviPixAdapter]
 })
 export class WooviModule {}
